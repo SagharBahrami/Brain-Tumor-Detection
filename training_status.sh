774ef046321e9e7f3babf1e1e0a8d798c717032c
SagharBahrami <68621932+SagharBahrami@users.noreply.github.com>
2026-02-02 11:29:52 -0800

    old files cleanup

--- training_status.sh
+++
@@ -1,9 +0,0 @@
-# Training Status Monitor
-echo '=== Training Status ==='
-date
-echo 'Running processes:'
-ps aux | grep 'train_model.py' | grep -v grep | wc -l
-echo 'Recent log activity:'
-tail -5 full_training.log
-echo 'Experiment directories:'
-ls -1 experiments_logs/ | grep 2025-11-27 | wc -l
